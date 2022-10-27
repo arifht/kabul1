d01aaf777e2f94d9e8f53598892ffb225ee9fa00
ZykoBotz-MD <113540643+ZykoBotz-MD@users.noreply.github.com>
2022-10-27 21:44:41 +0700

    kabul

---
+++ plugins/_antilink.js
@@ -0,0 +1,25 @@
+const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+export async function before(m, { isAdmin, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe)
+        return !0
+    if (!m.isGroup) return !1
+    let chat = global.db.data.chats[m.chat]
+    let name = conn.getName(m.sender)
+    let bot = global.db.data.settings[this.user.jid] || {}
+    const isGroupLink = linkRegex.exec(m.text)
+    let hapus = m.key.participant
+    let bang = m.key.id
+
+    if (chat.antiLink && isGroupLink && !isAdmin) {
+        if (isBotAdmin) {
+            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+            if (m.text.includes(linkThisGroup)) return !0
+        }
+        await conn.sendButton(m.chat, `Terdeteksi ${name} Telah Mengirim Link Grup lain!! ${isBotAdmin ? '' : '\n\n_Bot not admin_  t_t'}`, author, ['Owner', '/owner'], m)
+        if (isBotAdmin && bot.restrict) {
+        return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: hapus }})
+    //    return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+        } else if (!bot.restrict) return m.reply('*Anda Atmin Anda Aman :v!*')
+    }
+    return !0
+}
