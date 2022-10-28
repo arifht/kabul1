2ded755fc63dd19ef16839e41e78c38d27f83fe4
ZykoBotz-MD <113540643+ZykoBotz-MD@users.noreply.github.com>
2022-10-28 23:44:53 +0700

    kabul

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -7,7 +7,7 @@ if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup || db.data.settings[th
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let name = await conn.getName(who)
     let wan = 'WannBotz-MD'
-    let caption = `Halo ${name}, Saya Adalah ${nomorown}, ${user.banned ? 'kamu dibanned' : `Ada yang bisa ${nomorown}, ʙᴀɴᴛᴜ?\nᴋᴇᴛɪᴋ *.ᴍᴇɴᴜ* ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ sᴇᴍᴜᴀ ғɪᴛᴜʀ ʙᴏᴛ.\n\nᴊᴀɴɢᴀɴ ʟᴜᴘᴀ ᴊᴏɪɴ ᴍᴀsᴜᴋ ɢʀᴏᴜᴘ\n\nɢʀᴏᴜᴘ ᴢʏᴋᴏʙᴏᴛ-ᴍᴅ ᴏғғɪᴄɪᴀʟ :\nhttps://chat.whatsapp.com/B9PTc9PZbBH0INkdEN07ws`}
+    let caption = `Halo ${name}, Saya Adalah ${namebot}, ${user.banned ? 'kamu dibanned' : `Ada yang bisa ${namebot}, ʙᴀɴᴛᴜ?\nᴋᴇᴛɪᴋ *.ᴍᴇɴᴜ* ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ sᴇᴍᴜᴀ ғɪᴛᴜʀ ʙᴏᴛ.\n\nᴊᴀɴɢᴀɴ ʟᴜᴘᴀ ᴊᴏɪɴ ᴍᴀsᴜᴋ ɢʀᴏᴜᴘ\n\nɢʀᴏᴜᴘ ᴢʏᴋᴏʙᴏᴛ-ᴍᴅ ᴏғғɪᴄɪᴀʟ :\nhttps://chat.whatsapp.com/B9PTc9PZbBH0INkdEN07ws`}
 `.trim()
     this.sendButton(m.chat, caption, wm, null, [[user.banned ? 'Owner' : 'Menu', user.banned ? '/owner' : '/menu']], m, { mentions: this.parseMention(caption) })
     user.pc = new Date * 1
